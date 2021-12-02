4bda09353d3fa6498f3957eb67070df12382823a
Dayanna Lugo <87387605+alexanday@users.noreply.github.com>
2021-12-02 09:56:50 -0500

    Update bmiCalc.py

--- bmiCalc.py
+++ bmiCalc.py
@@ -1,7 +1,5 @@
 #calculate BMI
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
 
-height = float(height)
-weight = float(weight)
 print(int(weight/(height * height)))
